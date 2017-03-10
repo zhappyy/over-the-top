95309c4a64500c10edcbe2a77d2288e7afb3f2ba
liujian1 <liujian1@xiaomi.com>
2017-03-10 23:46:21 +0800

    ip

--- Overthetop/client/src/main/java/com/xiaomi/miui/myapplication/Global.java
+++ Overthetop/client/src/main/java/com/xiaomi/miui/myapplication/Global.java
@@ -4,5 +4,5 @@ package com.xiaomi.miui.myapplication;
  * Created by mi on 17-3-10.
  */
 public class Global {
-    public static final String SERVER_IP = "10.232.65.53";
+    public static final String SERVER_IP = "10.232.61.81";
 }
