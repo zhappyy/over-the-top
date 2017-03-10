ea7a3a84d189ac1dfe9d885ccd8ef0b1839cce1d
liujian1 <liujian1@xiaomi.com>
2017-03-10 09:06:08 +0800

    fix

--- Overthetop/client/src/main/java/com/xiaomi/miui/myapplication/sound/SoundActivity.java
+++ Overthetop/client/src/main/java/com/xiaomi/miui/myapplication/sound/SoundActivity.java
@@ -24,7 +24,7 @@ public class SoundActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sound);
         mRecorder = new MyMediaRecorder();
     }
 
